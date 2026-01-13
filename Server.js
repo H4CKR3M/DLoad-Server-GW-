d946e70c42c664fc76223a79b42ae53d76133ee6
H4CKR3M <121751918+H4CKR3M@users.noreply.github.com>
2026-01-12 16:37:38 -0800

    Changed "Weekly" challenge

--- Server.js
+++ Server.js
@@ -5,48 +5,43 @@ var SERVER = {
     DAILIES: [
         {
             TYPE: "WEEKLY",
-            REWARD_URL: "https://h4ckr3m.github.io/DLoad-Server-GW-/dlc/dlc02_Week1.js",
-            MESSAGE_LOGIN: "&cKill &e&oThe Roc &cusing only your starting weapon!",
-            MESSAGE_SUCCESS: "&a&lSUCCESS &a- Unique Trinket Added\n&7Kill &8The Roc&7 using only your starting weapon~",
-            MESSAGE_FAILURE: "Failed &7&m Kill &8&mThe Roc &7&musing only your starting weapon! ",
+            REWARD_URL: "https://h4ckr3m.github.io/DLoad-Server-GW-/dlc/dlc03_Week2.js",
+            MESSAGE_LOGIN: "&cDeal &e[&6&l30&e] &cor more damage in a single attack with an unleveled weapon!",
+            MESSAGE_SUCCESS: "&a&lSUCCESS &a- Unique Trinket Added\n" + "&7Deal &8[&7&l30&8] &7or more damage in a single attack with an unleveled weapon!",
+            MESSAGE_FAILURE: "FAILED! " + "&7Deal &8[&7&l30&8] &7or more damage in a single attack with an unleveled weapon!",
 
             isValidRun: true,
             saidLoginMessage: false,
 
-            Init: function Init(e) {},
+            Init: function Init(e) { },
             Tick: function Tick(e) {
-                if (e.player.getGamemode() == 1 && this.isValidRun){
+                if (e.player.getGamemode() == 1 && this.isValidRun) {
                     this.isValidRun = false;
                 }
             },
             Kill: function Kill(e) {
-                if (e.entity != null && e.entity.getType() == 2 && e.entity.getName() == "The Roc"){
-                    if (this.isValidRun && this.IsUsingStartingWeapon(e)){
-                        DLoad.CompleteDaily(this);
-                    }
+                if (e.entity != null && this.isValidRun && this.IsValidWeapon(e)) {
+                    DLoad.CompleteDaily(this);
                 }
             },
-            DamagedEntity: function DamagedEntity(e){
-                if (e.target != null && e.target.getType() == 2 && e.target.getName() == "The Roc"){
-                    if (!this.IsUsingStartingWeapon(e)){
-                        DLoad.FailDaily(this);
-                    }
+            DamagedEntity: function DamagedEntity(e) {
+                if (e.target != null && this.isValidRun && this.IsValidWeapon(e)) {
+                    DLoad.CompleteDaily(this);
                 }
             },
-            IsUsingStartingWeapon: function IsUsingStartingWeapon(e){
-                var mainhand = e.player.getMainhandItem();
-                if (mainhand != null && mainhand.getName() != "minecraft:air"){
-                    var tagObj = Utilities.GetItemTags(mainhand);
-                    if (tagObj != null){
-                        if (!Utilities.HasTag(tagObj, "STARTING")){
+            IsValidWeapon: function IsValidWeapon(e) {
+                var dmg = e.damage;
+                if (dmg >= 70) {
+                    var mainhand = e.player.getMainhandItem();
+                    if (mainhand != null && mainhand.getName() != "minecraft:air") {
+                        var tagObj = Utilities.GetItemTags(mainhand);
+                        if (tagObj != null && (Utilities.HasTag(tagObj, "LEVELEDLOW") || Utilities.HasTag(tagObj, "LEVELEDHIGH"))) {
                             return false;
                         }
                     }
-                    else{
-                        return false;
-                    }
+                    return true;
                 }
-                return true;
+                return false;
             }
         },
 
@@ -57,9 +52,9 @@ var SERVER = {
             REWARD_URL: "https://h4ckr3m.github.io/DLoad-Server-GW-/dlc/achv_thatsAllFolks.js",
             SUCCESS_MESSAGE: "That's All Folks!\n§7~Complete the R2 Demo Build of Giant Whoop",
 
-            Init: function Init(e) {},
+            Init: function Init(e) { },
             Tick: function Tick(e) {
-                if (e.player.x > 1006 && e.player.x < 1039 && e.player.y > 81 && e.player.y < 93 && e.player.z > 964 && e.player.z < 988){
+                if (e.player.x > 1006 && e.player.x < 1039 && e.player.y > 81 && e.player.y < 93 && e.player.z > 964 && e.player.z < 988) {
                     DLoad.CompleteDaily(this);
                 }
             }
@@ -71,19 +66,19 @@ var SERVER = {
 
             isValidRun: true,
 
-            Init: function Init(e) {},
+            Init: function Init(e) { },
             Tick: function Tick(e) {
-                if (e.player.getGamemode() == 1 && this.isValidRun){
+                if (e.player.getGamemode() == 1 && this.isValidRun) {
                     e.player.world.getStoreddata().put("FLAWLESS_X" + e.player.getDisplayName(), 1);
                     this.isValidRun = false;
                 }
 
-                if (this.isValidRun && e.player.x > 1006 && e.player.x < 1039 && e.player.y > 81 && e.player.y < 93 && e.player.z > 964 && e.player.z < 988){
+                if (this.isValidRun && e.player.x > 1006 && e.player.x < 1039 && e.player.y > 81 && e.player.y < 93 && e.player.z > 964 && e.player.z < 988) {
                     DLoad.CompleteDaily(this);
                 }
             },
             Login: function Login(e) {
-                if(e.player.world.getStoreddata().has("FLAWLESS_X" + e.player.getDisplayName())){
+                if (e.player.world.getStoreddata().has("FLAWLESS_X" + e.player.getDisplayName())) {
                     this.isValidRun = false;
                 }
             },
@@ -93,7 +88,7 @@ var SERVER = {
             }
         },
     ],
-    INIT: function INIT(){
+    INIT: function INIT() {
         DLoad.DownloadContent("https://h4ckr3m.github.io/DLoad-Server-GW-/dlc/dlc01_First100.zip");
     }
 }
---
+++ dlc/dlc03_Week2.js
@@ -0,0 +1,63 @@
+/* Weekly (02) - Completion Reward | Load Script Through DLoad | Minecraft 1.12.2 (05Jul20) | Written by Rimscar */
+var DLC = {
+    DLOAD_VERSION: 1,
+    CATEGORY: "TRINKET",
+    CHALLENGE_NUMBER: 1,
+    ITEMS: [
+        "§e[§6§lDLC§e] §a[§2§lULTRA§a] §2Chest §a[§20§a/§21000§a]@0minecraft:skull@1§6§l§o* Legendary §c[Trinket]@1§7@1§8A Chest cursed by the old@1§8ones, it cannot be opened@1§8without sufficient tribute.@1§9@1§7When in off hand:@1§9  > Slay Foes to Fill Chest@1§9 [§eRight-Click§9]@1§7  > When Full?@1§9  * Give Ultra §kDAGGER@2SCRIPTED@2TRINKET@2DAILY@2OFFHAND@2ULTRACHEST@2ULTRAINGREDIENT@2DLC@2INGREDIENT@2LEGENDARY@46bfb34fb-17bf-4104-ba2c-5eea9757ce66$eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjJlZTkyOWU2NDEwN2ZmYjVmYjk0NTAwZmM2NDAyZGFmZDE3ZjNiNGIxOGYxZjliMmM4YzAxZWQ5YWRhZGNjYiJ9fX0=@63"
+    ],
+    TRINKET_TAG: "DAILY",
+    TRINKET_SCRIPTS_OFFHAND: [
+        {
+            tag: "ULTRACHEST",
+            rewardStr: "§e[§6§lDLC§e] §a[§2§lULTRA§a] §2Dagger@0variedcommodities:emerald_dagger_reversed@1§6§l§o* Legendary@1§7@1§aA dagger with an errie@1§aradiating glow, said to@1§ahave been lost long ago.@1§9@1§7When in main hand:@1§9  No Attack Cooldown@1§9  20 Attack Damage@1§9  Sneaking = [§ex§l2§9] DMG@1§9  +20 XP on Kill@1§c  Consumes 1 XP/sec@1§9@1§9Unbreakable@2DAILY@2MAINHAND@2INGREDIENT@2SCRIPTED@2ULTRADAGGER@2ULTRAINGREDIENT@2DLC@2LEGENDARY@3100.0$mainhand$generic.attackSpeed$-7339881495005740422L$-7246337421202354335L@320.0$mainhand$generic.attackDamage$5611130981972593098L$-4744572898023570227L@57@71@C34$1",
+            Interact: function Interact(e) {
+                var offhand = e.player.getOffhandItem();
+                if (offhand != null && offhand.getName() != "minecraft:air") {
+                    var tagObj = Utilities.GetItemTags(offhand);
+                    if (tagObj != null && Utilities.HasTag(tagObj, "ULTRACHEST")) {
+                        var name = offhand.getDisplayName();
+                        var indexStart = name.indexOf("Chest") + 11;
+                        var indexEnd = name.indexOf("§a/§21000");
+                        var count = Number(name.substring(indexStart, indexEnd));
+                        Utilities.Br
+                        if (count >= 1000){
+                            offhand.setStackSize(0);
+                            DigitalTrinkets12.Give(e.player, this.rewardStr);
+                            Utilities.Play(e.player, "minecraft:block.end_portal.spawn");
+                        }
+                    }
+                }
+            },
+            Kill: function Kill(e) {
+                var offhand = e.player.getOffhandItem();
+                if (offhand != null && offhand.getName() != "minecraft:air") {
+                    var tagObj = Utilities.GetItemTags(offhand);
+                    if (tagObj != null && Utilities.HasTag(tagObj, "ULTRACHEST")) {
+                        var name = offhand.getDisplayName();
+                        var indexStart = name.indexOf("Chest") + 11;
+                        var indexEnd = name.indexOf("§a/§21000");
+                        var count = Number(name.substring(indexStart, indexEnd));
+                        count += 150;
+                        count = Math.min(count, 1000);
+                        offhand.setCustomName("§e[§6§lDLC§e] §a[§2§lULTRA§a] §2Chest §a[§2" + count + "§a/§21000§a]");
+                    }
+                }
+            }
+        },
+    ],
+    TRINKET_SCRIPTS_MAINHAND: [
+        {
+            tag: "ULTRADAGGER",
+            DamagedEntity: function DamagedEntity(e) {
+                if (e.player.getExpLevel() > 0 && e.player.isSneaking()){
+                    e.damage != 2;
+                }
+            },
+            Kill: function Kill(e) {
+                var lvl = e.player.getExpLevel();
+                e.player.setExpLevel(lvl + 20);
+            },
+        },
+    ]
+}
